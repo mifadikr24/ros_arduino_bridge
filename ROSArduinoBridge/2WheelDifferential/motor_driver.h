5b00174351a0e3916ca32abc2b6e602d1e653c10
mifadikr24 <add.ikr24ubuntu@gmail.com>
2024-05-28 21:58:19 +0700

    Change PIN 2WD

--- ROSArduinoBridge/2WheelDifferential/motor_driver.h
+++ ROSArduinoBridge/2WheelDifferential/motor_driver.h
@@ -3,10 +3,11 @@
    *************************************************************/
 
 #ifdef L298_MOTOR_DRIVER
-  #define RIGHT_MOTOR_BACKWARD 5
-  #define LEFT_MOTOR_BACKWARD  6
-  #define RIGHT_MOTOR_FORWARD  9
-  #define LEFT_MOTOR_FORWARD   10
+  #define RIGHT_MOTOR_BACKWARD 11
+  #define LEFT_MOTOR_BACKWARD  9
+  #define RIGHT_MOTOR_FORWARD  10
+  #define LEFT_MOTOR_FORWARD   6
+  
   #define RIGHT_MOTOR_ENABLE 12
   #define LEFT_MOTOR_ENABLE 13
 #endif
