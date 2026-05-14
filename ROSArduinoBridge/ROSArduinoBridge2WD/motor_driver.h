4eceabc76064097e317a7349be02ddad6a299f65
mifadikr24 <add.ikr24ubuntu@gmail.com>
2026-05-14 12:44:47 +0700

    change pin configuration

--- ROSArduinoBridge/ROSArduinoBridge2WD/motor_driver.h
+++ ROSArduinoBridge/ROSArduinoBridge2WD/motor_driver.h
@@ -13,9 +13,9 @@ MODIFIED 2026-05-12 for Mifad's 2WD configuration:
   OUT3 -> RL-
   OUT4 -> RL+
   ********************************************************/
-  #define LEFT_MOTOR_FORWARD 10
-  #define LEFT_MOTOR_BACKWARD 11
-  #define LEFT_MOTOR_ENABLE 8
+  #define LEFT_MOTOR_FORWARD 6
+  #define LEFT_MOTOR_BACKWARD 9
+  #define LEFT_MOTOR_ENABLE 13
 
   /********************************************************
   DRIVER 2 - REAR RIGHT (RR)
@@ -26,9 +26,9 @@ MODIFIED 2026-05-12 for Mifad's 2WD configuration:
   OUT3 -> RR-
   OUT4 -> RR+
   ********************************************************/
-  #define RIGHT_MOTOR_FORWARD 9
-  #define RIGHT_MOTOR_BACKWARD 6
-  #define RIGHT_MOTOR_ENABLE 13
+  #define RIGHT_MOTOR_FORWARD 10
+  #define RIGHT_MOTOR_BACKWARD 11
+  #define RIGHT_MOTOR_ENABLE 8
 
 #endif
 
